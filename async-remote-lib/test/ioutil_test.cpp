1756acbe106ce08ab611e048749e8708c5f00112
xiaomi388 <xiaomi388@gmail.com>
2021-04-12 22:00:38 -0700

    add one more test

--- async-remote-lib/test/ioutil_test.cpp
+++ async-remote-lib/test/ioutil_test.cpp
@@ -4,19 +4,35 @@
 
 #include <iostream>
 #include <string>
+#include <fstream>
 
 #include "ioutil.h"
 #include "dslice.h"
 #include "dstring.h"
 #include "gtest/gtest.h"
 
-using namespace std;
+using std::stringstream;
+using std::ofstream;
 
 TEST(ReadStreamTest, Normal) {
   stringstream ss;
   ss << "hello world foo\nhehe,";
   auto sli = ReadStream(ss);
 
+  string exps[] = {"hello", "world", "foo", "hehe,"};
+  ASSERT_EQ(4, GetSliceSize(sli));
+  for (int i = 0; i < GetSliceSize(sli); ++i) {
+    auto dstr = GetSliceIndexString(sli, i);
+    ASSERT_EQ(exps[i], GetCStr(dstr));
+  }
+}
+
+TEST(ReadFileTest, Normal) {
+  ofstream f("/tmp/test.txt");
+  f << "hello world\n foo hehe,\n";
+  f.close();
+
+  auto sli = ReadFile("/tmp/test.txt");
   string exps[] = {"hello", "world", "foo", "hehe,"};
   ASSERT_EQ(4, GetSliceSize(sli));
   for (int i = 0; i < GetSliceSize(sli); ++i) {
