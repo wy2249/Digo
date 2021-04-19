f954d0a88aff93f3e7a5f98563f4583bfd1a3c85
sdh21 <78904432+sdh21@users.noreply.github.com>
2021-04-19 21:40:49 +0800

    improve logs for testall

--- digo-test/testall.sh
+++ digo-test/testall.sh
@@ -36,8 +36,9 @@ global_test_error=0
 global_test_error_any=0
 
 global_log="testall.log"
+global_llvm_ir_output_log="testall_ir.log"
 rm -f $global_log
-
+rm -f $global_llvm_ir_output_log
 
 # Compare <outfile> <reffile> <difffile>
 # Compares the outfile with reffile. Differences, if any, written to difffile
@@ -66,6 +67,10 @@ RunTest() {
 
     ../digo-compiler/digo.native "$test_src" > ../tmp.compiled.nometadata.ll 2>"$test_name.build.output"
 
+    echo "" >> "$global_llvm_ir_output_log"
+    echo "##### Testing $test_name" >> "$global_llvm_ir_output_log"
+    cat ../tmp.compiled.nometadata.ll >> $global_llvm_ir_output_log
+
     errorlevel=$?
 
     if [ $errorlevel -eq 0 ] ; then
@@ -80,24 +85,39 @@ RunTest() {
 
     if [ $errorlevel -eq 0 ] ; then
         # success expected, so try to run the executable
-        ../executable --master 127.0.0.1:20001 > "$test_name.exec.output" 2>/dev/null
-        expected_file="$test_src.pass.expected"
         exec_output="$test_name.exec.output"
+        expected_file="$test_src.pass.expected"
+
+        ../executable --master 127.0.0.1:20001 > "$exec_output" 2>/dev/null
+
+        echo " ## Executable output: " >> "$global_log"
+        cat "$exec_output" >> "$global_log"
+
         if [ ! -f $expected_file ]; then
-            echo "Compilation passed, but we cannot find $expected_file" >> "$global_log"
+            echo " ## Compilation passed, but we cannot find $expected_file" >> "$global_log"
             global_test_error=1
         else
             Compare "$exec_output" "$expected_file" "$diff_output_file"
+
+            echo " ## Diff output: " >> "$global_log"
+            cat "$diff_output_file" >> "$global_log"
         fi
     else
         # fail expected, so try to diff the fail file
         expected_file="$test_src.fail.expected"
         build_output="$test_name.build.output"
+        
+        echo " ## Build output: " >> "$global_log"
+        cat "$build_output" >> "$global_log"
+
         if [ ! -f $expected_file ]; then
-            echo "Compilation failed, but we cannot find $expected_file" >> "$global_log"
+            echo " ## Compilation failed, but we cannot find $expected_file" >> "$global_log"
             global_test_error=1
         else
             Compare "$build_output" "$expected_file" "$diff_output_file"
+            
+            echo " ## Diff output: " >> "$global_log"
+            cat "$diff_output_file" >> "$global_log"
         fi
     fi
 
@@ -139,7 +159,12 @@ done
 
 shift `expr $OPTIND - 1`
 
-# BuildCompiler
+
+if [ ! -f ../digo-compiler/digo.native ]; then
+    echo "----------------- !!! Compiler not found ----------------------"
+    BuildCompiler
+fi
+
 
 if [ $# -ge 1 ]
 then
@@ -154,7 +179,6 @@ done
 
 
 if [ $keep -eq 0 ] ; then
-    pwd
     echo "Cleaning..."
     Clean
 fi
